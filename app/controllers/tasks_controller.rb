627983faab0c4da3f1ba9015b40de43b53e4531b
Hien Nguyen <mhien.ng@gmail.com>
2018-08-07 17:09:54 +0200

    completed tails task manager app

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,65 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to @task
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      redirect_to @task
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
+
+# INDEX
+# As a user, I can list tasks
+
+# SHOW
+# As a user, I can view the details of a task
+
+# CREATE
+# As a user, I can add a new task
+
+# EDIT
+# As a user, I can edit a task (mark as completed / update title & details)
+
+# REMOVE
+# As a user, I can remove a task
